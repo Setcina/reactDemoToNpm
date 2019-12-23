effe7ecc95cd6a9fda13ff51341af275fa529040
Setcina <1349855697@qq.com>
2019-12-23 16:31:28 +0800

    react-hook

--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './styles.css';
 
-class Hello extends React.Component {
-  render () {
-    return (
-      <h1>Hello world</h1>
-    );
-  }
+export const Hello = () => {
+  const t = useRef(null);
+  const [name, setName] = useState("ajanuw");
+  useEffect(() => {
+    t.current = name;
+    console.log(t);
+  });
+  const prevName = t.current;
+  return (
+    <div>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <h2>{name}</h2>
+      <p>{prevName}</p>
+    </div>
+  );
 }
 
 export default Hello;
